dd15ea4ead1047853eeaed8d5f1f96867c241ba2
npwpck <41568914+npwpck@users.noreply.github.com>
2018-08-28 22:24:37 +0700

    jopaeng

--- connect/connectdb.php
+++ connect/connectdb.php
@@ -3,7 +3,7 @@
    $host        = "host = 127.0.0.1";
    $port        = "port = 5432";
    $dbname      = "dbname = db_herbbb";
-   $credentials = "user = postgres password=jjsci1234";
+   $credentials = "user = postgres password=1234";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
